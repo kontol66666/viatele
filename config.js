3087ebf93e425dee9d01d8c4961d9d5327a733c2
kontol66666 <akagaminojeki39@gmail.com>
2025-01-01 22:10:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  BOT_TOKEN: "7594128672:AAFMyOGzikuNad7gVenUIGXXyPPjPntPN61Uw", // Token bot Telegram
-  OWNER_ID: "7340632755", // ID pemilik bot
+  BOT_TOKEN: "7660240073:AAEsfvsbCnADL3a_GQLCOTqPm33dSbkQORw", // Token bot Telegram
+  OWNER_ID: "6521470375", // ID pemilik bot
   allowedGroupIds: [-1002432677888, -1002361556670, -1002386380830, -1002415460020, -4514397992], // ID grup yang diizinkan
 };
