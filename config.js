e89436762081496935a65eac443c1f3e924d0f2c
kontol66666 <akagaminojeki39@gmail.com>
2025-01-01 22:09:33 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  BOT_TOKEN: "7594128672:AAFMyOGzikuNad7gVenUIGXXyPPjPntPN61Uw", // Token bot Telegram
+  OWNER_ID: "7340632755", // ID pemilik bot
+  allowedGroupIds: [-1002432677888, -1002361556670, -1002386380830, -1002415460020, -4514397992], // ID grup yang diizinkan
+};
